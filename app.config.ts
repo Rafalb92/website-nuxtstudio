f9b3b53d036f5e382b4aff6c7b9957394eb01a15
Rafalb92 <bochniewiczrafal@gmail.com>
2024-06-09 18:46:50 +0200

    First upload nuxt + ui + taiwlid

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    button: {
+      base: "green",
+      color: "green",
+      default: "green",
+      size: "lg",
+      variant: {
+        solid: "green",
+      },
+    },
+  },
+});
